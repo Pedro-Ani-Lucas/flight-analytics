b893bddb158f9481917622cf8a7deba9c8fba5e9
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-09-16 14:50:05 -0300

    feat(clean_datas): Adição de comandos para tratamento de dados do arquivo csv

--- src/clean_datas.py
+++ src/clean_datas.py
@@ -4,3 +4,20 @@ from pathlib import Path
 pasta_do_script = Path(__file__).resolve().parent
 caminho_csv = pasta_do_script.parent / "data" / "flights_raw.csv"
 
+df = pd.read_csv(caminho_csv)
+
+# 1 - Resolver a coluna Airline
+
+df["airline"] = df["airline"].str.strip().str.upper()
+
+# 2 - Resolver a coluna Delay Minutes sobre o valor NULL e sobre os valores anormais
+
+df_limpo = df[(~df["delay_minutes"].isna()) & 
+            (df["delay_minutes"] >= 0) & 
+            (df["delay_minutes"] != 999)]
+
+# 3 - Resolver a coluna Passengers sobre o valor NULL
+
+df_limpo["passengers"] = df[~df["delay_minutes"].isna()]
+
+
