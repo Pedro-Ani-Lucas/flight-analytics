4d7d7e42ce460b280fec43183086cc279acbaaed
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-09-16 19:13:11 -0300

    feat(clean-datas): Implementação de função de limpeza

--- src/clean_datas.py
+++ src/clean_datas.py
@@ -6,17 +6,18 @@ caminho_csv = pasta_do_script.parent / "data" / "flights_raw.csv"
 
 df = pd.read_csv(caminho_csv)
 
-# 1 - Resolver a coluna Airline
+def limpeza_registros_voos(df):
+    df = df.copy()
 
-df["airline"] = df["airline"].str.strip().str.upper()
+    # 1 - Resolver a coluna Airline
+    df["airline"] = df["airline"].str.strip().str.upper()
 
-# 2 - Resolver a coluna Delay Minutes sobre o valor NULL e sobre os valores anormais
-
-df_limpo = df[(~df["delay_minutes"].isna()) & 
+    # 2 - Resolver a coluna Delay Minutes sobre o valor NULL e sobre os valores anormais
+    df = df[(~df["delay_minutes"].isna()) & 
             (df["delay_minutes"] >= 0) & 
             (df["delay_minutes"] != 999)]
 
-# 3 - Resolver a coluna Passengers sobre o valor NULL
+    # 3 - Resolver a coluna Passengers sobre o valor NULL
+    df_limpo = df[~df["passengers"].isna()]
 
-df_limpo = df_limpo[~df_limpo["passengers"].isna()]
-
+    return df_limpo 
