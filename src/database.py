097145954284a0d2bb5981bfa53bef1a22fd24ca
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-09-17 16:47:45 -0300

    feat(database): Separação de pastas, criação do database e melhorias.

--- src/database.py
+++ src/database.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 pasta_do_script = Path(__file__).resolve().parent
 
-caminho_db_flights = pasta_do_script.parent / "data" / "database" / "flights.db"
+caminho_db_flights = pasta_do_script.parent / "database" / "flights.db"
 caminho_db_flights.parent.mkdir(parents=True, exist_ok=True)
 
 caminho_csv_flights_limpo = pasta_do_script.parent / "data" / "csv" / "flights_limpo.csv"
