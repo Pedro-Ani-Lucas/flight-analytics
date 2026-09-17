c154bfcad00118147c034696e5fb0cf3cc57fc79
Pedro-Ani-Lucas <aniwoucontato@gmail.com>
2026-09-17 15:02:00 -0300

    feat(main): Implementando um processo de ler, tratar, receber e imprimir informações.

--- src/clean_datas.py
+++ src/clean_datas.py
@@ -4,20 +4,18 @@ from pathlib import Path
 pasta_do_script = Path(__file__).resolve().parent
 caminho_csv = pasta_do_script.parent / "data" / "flights_raw.csv"
 
-df = pd.read_csv(caminho_csv)
-
 def limpeza_registros_voos(df):
-    df = df.copy()
+        df = df.copy()
 
-    # 1 - Resolver a coluna Airline
-    df["airline"] = df["airline"].str.strip().str.upper()
+        # 1 - Resolver a coluna Airline
+        df["airline"] = df["airline"].str.strip().str.upper()
 
-    # 2 - Resolver a coluna Delay Minutes sobre o valor NULL e sobre os valores anormais
-    df = df[(~df["delay_minutes"].isna()) & 
-            (df["delay_minutes"] >= 0) & 
-            (df["delay_minutes"] != 999)]
+        # 2 - Resolver a coluna Delay Minutes sobre o valor NULL e sobre os valores anormais
+        df = df[(~df["delay_minutes"].isna()) & 
+        (df["delay_minutes"] >= 0) & 
+        (df["delay_minutes"] != 999)]
 
-    # 3 - Resolver a coluna Passengers sobre o valor NULL
-    df_limpo = df[~df["passengers"].isna()]
+        # 3 - Resolver a coluna Passengers sobre o valor NULL
+        df_limpo = df[~df["passengers"].isna()]
 
-    return df_limpo 
+        return df_limpo 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from pathlib import Path
+from clean_datas import limpeza_registros_voos
 
 pasta_do_script = Path(__file__).resolve().parent
 caminho_csv_flights = pasta_do_script.parent / "data" / "flights.csv"
 
 caminho_csv_flights_raw = pasta_do_script.parent / "data" / "flights_raw.csv"
 
+df_sujo = pd.read_csv(caminho_csv_flights_raw)
+
+df_limpo = limpeza_registros_voos(df_sujo)
+
+print(f"Quantidade de registros: {len(df_sujo)}")
+print(f"Quantidade de registros: {len(df_limpo)}")
+print(f"Quantidade de registros removidos: {len(df_sujo)-len(df_limpo)}")
